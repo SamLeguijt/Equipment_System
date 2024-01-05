0681c54fffdbb883ed76745fa1ae6055c3e5e054
SamLeguijt <sam.leguijt@gmail.com>
2024-01-05 13:48:52 +0100

    Added PlayerMovement code. Player can now move in all directions facing the way its looking. Needs speed control and optimizing

--- Christmas Countdown/Assets/Scripts/Player/PlayerMovement.cs
+++ Christmas Countdown/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Rigidbody rb; 
+    private Rigidbody rb;
 
-    
+    [SerializeField ]private float movementSpeed;
+
+    [SerializeField] private Transform playerOrientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDirection; 
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckMoveInput();
+        Move(); 
+    }
+
+    private void Move()
+    {
+        moveDirection = ((playerOrientation.forward * verticalInput) + (playerOrientation.right * horizontalInput)).normalized;
+        rb.AddForce(moveDirection * movementSpeed, ForceMode.Force);
+    }
+
+    private void CheckMoveInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
     }
 }
