1026b8ea308658161270e92f4d5918c7fc4d198b
SamLeguijt <sam.leguijt@gmail.com>
2024-01-06 21:46:47 +0100

    First iteration of equipping an item in either players' hands. Item can be put in the hand, but needs optimization and global usage

---
+++ Christmas Countdown/Assets/Scripts/EquipmentBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentBehaviour : MonoBehaviour
+{
+
+    private BaseEquipmentObject equipmentData;
+
+    private bool isEquipped;
+
+    public bool IsEquipped
+    {
+        get { return isEquipped; }
+        set { isEquipped = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Christmas Countdown/Assets/Scripts/EquipmentSystemController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSystemController : MonoBehaviour
+{
+
+    public GameObject target;
+    public Vector3 inHandPositionLeft;
+    public Vector3 inHandPositionRight;
+
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public float equipDistance;
+
+    private EquipmentBehaviour currentEquipmentLeft;
+    private EquipmentBehaviour currentEquipmentRight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (DistanceFromPlayerToTarget(target) < equipDistance)
+            {
+                // TODO: FIX DROPPING ITEM FROM EQUIPPED HAND 
+                if (currentEquipmentLeft == null)
+                {
+                    Equip(target, leftHand);
+                }
+                else if (currentEquipmentRight == null )
+                {
+                    Equip(target, rightHand);
+                }
+                else
+                {
+                    Drop(target);
+                }
+            }
+        }
+    }
+
+    private float DistanceFromPlayerToTarget(GameObject _target)
+    {
+        return (Vector3.Distance(gameObject.transform.position, _target.transform.position));
+    }
+
+    private void Equip(GameObject _item, Transform _hand)
+    {
+        if (!_item.GetComponent<EquipmentBehaviour>()) { return; }
+        else
+        {
+            _item.transform.position = _hand.transform.position;
+            _item.gameObject.transform.SetParent(_hand, true);
+        }
+    }
+
+    private void Drop(GameObject _item)
+    {
+        _item.transform.parent = null;
+        _item.transform.position = gameObject.transform.position;
+    }
+}
