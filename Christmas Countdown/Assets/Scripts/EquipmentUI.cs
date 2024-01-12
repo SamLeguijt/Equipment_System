1f848d879ea06e02ca5e7a895bafd2faa8f3997c
SamLeguijt <sam.leguijt@gmail.com>
2024-01-12 14:06:46 +0100

    Cleaned up code, added comments to methods, renamed variables and methods, reorganized methods, added properties and more tooltips.

--- Christmas Countdown/Assets/Scripts/EquipmentUI.cs
+++ Christmas Countdown/Assets/Scripts/EquipmentUI.cs
@@ -24,6 +24,12 @@ public class EquipmentUI : MonoBehaviour
     [Tooltip("Desired position of the RectTransform of the TMP object")]
     [SerializeField] private Vector3 rectTargetPosition;
 
+    [Tooltip("The word being displayed as text when hand is free ( {inputKey} to x Left hand/Right hand")]
+    [SerializeField] private string equipActionWord;
+
+    [Tooltip("The word being displayed as text when hand is not free ( {inputKey} to x Left hand/Right hand")]
+    [SerializeField] private string swapActionWord; 
+
     // The current equipment being targeted by the UI (mouse, name, collider)
     private EquipmentBehaviour currentEquipmentTarget;
 
@@ -47,7 +53,7 @@ public class EquipmentUI : MonoBehaviour
     /// </summary>
     public TextMeshProUGUI TextObject
     {
-        get { return textObject; }  
+        get { return textObject; }
     }
 
     /// <summary>
@@ -98,153 +104,173 @@ public class EquipmentUI : MonoBehaviour
         get { return interactInstructionsString; }
     }
 
+    /// <summary>
+    /// Public property to get or set the word being displayed to equip/pick up equipment in either hand
+    /// </summary>
+    public string EquipActionWord
+    {
+        get { return equipActionWord; }
+        set { equipActionWord = value; }    
+    }
+
+    /// <summary>
+    /// Public property to get or set the word being displayed to swap an equipment in either hand
+    /// </summary>
+    public string SwapActionWord
+    {
+        get { return swapActionWord; }
+        set { swapActionWord = value; } 
+    }
+
+
 
     /* ------------------------------------------  METHODS ------------------------------------------- */
 
 
-
+    /// <summary>
+    /// Handles getting components and Initializing the UI object, or else logging an error and disabling the gameobject
+    /// </summary>
     private void Start()
     {
-        InitializeEquipmentUI();
-    }
-
-    public void InitializeEquipmentUI()
-    {
+        // Get RectTransform from TMP object
         rectTransform = textObject.GetComponent<RectTransform>();
 
+        if (textObject != null && rectTransform != null && controller != null)
+        {
+            // Call method to initialize if all references are found 
+            InitializeEquipmentUI();
+        }
+        else // One or more missing references, log error and disable this object
+        {
+            Debug.LogError($"One or more references from {gameObject.name} is null! Disabling this object, rerun with assigned references!");
+            gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Method that sets up the script by getting components and setting values of the transform
+    /// </summary>
+    public void InitializeEquipmentUI()
+    {
+        // Set the RectTransform properties to set UI text at correct position in game view
         SetTransformProperties();
-
-        DisableEquipmentUI();
-    }
-
-
-    private bool IsMouseOverCollider(Collider _colliderToCheck)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit) && hit.collider == _colliderToCheck)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-
-    private void Update()
-    {
-        if (currentEquipmentTarget != null)
-        {
-            if (!IsMouseOverCollider(currentEquipmentTarget.MouseDetectCollider))
-            {
-                currentEquipmentTarget = null;
-                DisableEquipmentUI();
-            }
-            else
-            {
-                if (currentEquipmentTarget.IsOnGround)
-                {
-                    EnableEquipmentUI(currentEquipmentNameToDisplay, interactInstructionsString);
-                }
-            }
-        }
-   
     }
 
 
     /// <summary>
-    /// Public method to enable the UI text for equipment <br/>
-    /// Specific for equipment only, because it updates the text obj to the name of equipment and equipText string
+    /// Handles the text being displayed 
     /// </summary>
-    public void EnableEquipmentUI(string _equipmentName, string _instructionsText)
+    private void Update()
     {
-        if (isTextEnabled)
+        // Return immediatily if there is no equipment target
+        if (currentEquipmentTarget == null) return;
+        else // There is a target
         {
-            return;
+            // Check if the mouse is over the MouseDetectCollider of the equipment, and if the equipment is grounded (prevent ui display when object is in air)
+            if (IsMouseOverCollider(currentEquipmentTarget.MouseDetectCollider) && (currentEquipmentTarget.IsOnGround))
+            {
+                // Enable the UI text by calling method, passing the current equipment name and the interact string as text
+                EnableEquipmentUI(currentEquipmentNameToDisplay, interactInstructionsString);
+
+            }
+            else // Mouse is not over collider, or equipment is not grounded so dont displlay text 
+            {
+                // Remove the target (enables early return)
+                currentEquipmentTarget = null;
+
+                // Call method to disable the UI text
+                DisableEquipmentUI();
+            }
         }
-        else
+    }
+
+    /// <summary>
+    ///  Public method to enable the equipment UI text <br/>
+    ///  Receives two parameters as display text
+    /// </summary>
+    /// <param name="_equipmentName"> Name of the equipment to display </param>
+    /// <param name="_interactInstructions"> String holding instructions for interacting </param>
+    private void EnableEquipmentUI(string _equipmentName, string _interactInstructions)
+    {
+        // Prevent unnecessary calls if already enabled 
+        if (isTextEnabled) return;
+        else // Text is not yet enabled
         {
-            UpdateEquipmentText(_equipmentName, _instructionsText);
+            // Call method to display the new text, passing in both strings
+            UpdateTextBox(_equipmentName, _interactInstructions, true); // True to add a new line between strings
+
+            // Set bool for early returns if more calls to this method
             isTextEnabled = true;
         }
     }
 
+
     /// <summary>
-    /// Public method to Disable the UI text for the equipment <br/>
-    /// Specific for equipment UI only, bacause it updates the text to empty strings 
+    /// Method that disables the equipment UI text by updating the text with empty strings
     /// </summary>
-    public void DisableEquipmentUI()
+    private void DisableEquipmentUI()
     {
-        if (!isTextEnabled)
+        // Prevent unnecessary calls if already disabled 
+        if (!isTextEnabled) return;
+        else // Not disabled yet
         {
-            return;
-        }
-        else
-        {
-            UpdateEquipmentText(string.Empty, string.Empty);
+            // Call method to update the text box with empty strings (works better than disabling the object, that causes lag on re-enabling)
+            UpdateTextBox(string.Empty, string.Empty);
+
+            // Set bool for early returns when calling method again without changes
             isTextEnabled = false;
         }
     }
 
-    private string GetInstructionsString()
+    /// <summary>
+    /// Method that updates the CurrentEquipment to a new equipment object <br/>
+    /// Sets name to new equipments name, and gets a new Instruction string to display
+    /// </summary>
+    /// <param name="_newEquipment"> The equipment info being used to update the variables, called in EquipmentBehaviour class upon mouse collision and in range </param>
+    public void UpdateEquipmentInfo(EquipmentBehaviour _newEquipment)
     {
-        string leftHandText = GetEquipOrSwapString(controller.LeftHand);
-        string rightHandText = GetEquipOrSwapString(controller.RightHand);
-
-        string fullString = $" {controller.LeftHandInputKey} {leftHandText} Left   |   {controller.RightHandInputKey} {rightHandText} Right";
-
-        return fullString;
-    }
-
-    public void UpdateEquipmentInfo(EquipmentBehaviour _equipment)
-    {
-        if (currentEquipmentTarget != _equipment)
+        // Prevent unnecessary calls if new equipment is already the current target
+        if (currentEquipmentTarget != _newEquipment)
         {
-            currentEquipmentTarget = _equipment;
+            // If not, set it to the current target first
+            currentEquipmentTarget = _newEquipment;
 
-            // Displlay the equipments name
-            currentEquipmentNameToDisplay = _equipment.EquipmentData.EquipmentName;
+            // Store the name of the new equipment by looking at its scriptable object
+            currentEquipmentNameToDisplay = _newEquipment.EquipmentData.EquipmentName;
 
-            interactInstructionsString = GetInstructionsString();
+            // Update the InteractInstruction string to display based on hand status'
+            interactInstructionsString = GetInteractionInstructionString();
         }
-        else
+        else // _newEquipment is already the current target, so no need to update everything
         {
             return;
         }
     }
 
+
     /// <summary>
-    /// Returns a string either saying "Equip" or "Swap" based on current hand equipment for specified hand
+    /// Method that sets the text of the TMP object to the two input strings params
     /// </summary>
-    /// <param name="_hand"></param>
-    /// <returns></returns>
-    private string GetEquipOrSwapString(Hand _hand)
+    /// <param name="_string1"> First string to display </param>
+    /// <param name="_string2"> Second string to display</param>
+    /// <param name="_seperateLine"> Should the strings be seperated by a new line? Default to true </param>
+    private void UpdateTextBox(string _string1, string _string2, bool _seperateLine = true)
     {
-        if (_hand.CurrentEquipment == null)
+        // Should be seperated by new line
+        if (_seperateLine)
         {
-            return "Equip";
+            // Set text with a new line between the string
+            textObject.SetText($"{_string1} \n {_string2}");
         }
-        else
+        else // Should not be seperate, just one full string
         {
-            return "Swap";
+            // Set text by making one line
+            textObject.SetText($"{_string1} {_string2}");
         }
     }
 
     /// <summary>
-    /// Method that sets the text of the Text object to the given parameters
-    /// </summary>
-    /// <param name="_equipmentName"> name of the equipment to display</param>
-    /// <param name="_instructionsText"> String to display under the equipment name </param>
-    private void UpdateEquipmentText(string _equipmentName, string _instructionsText)
-    {
-        textObject.SetText($"{_equipmentName} \n {_instructionsText} ");
-    }
-
-
-
-    /// <summary>
-    /// Method that sets the transform properties of the Text object to place in right position
+    /// Method that sets the transform properties of the Text object to place in the right position
     /// </summary>
     private void SetTransformProperties()
     {
@@ -254,5 +280,69 @@ public class EquipmentUI : MonoBehaviour
 
         // Set the position to the desired target position (position based on anchors)
         rectTransform.anchoredPosition = rectTargetPosition;
+    }
+
+    /// <summary>
+    /// Returns true if the mouse is over the param collider
+    /// </summary>
+    /// <param name="_colliderToCheck"></param>
+    /// <returns></returns>
+    private bool IsMouseOverCollider(Collider _colliderToCheck)
+    {
+        // Shoot a ray from the camera through the mouse 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // If the ray hits something, and the collider of the hit is same as our param
+        if (Physics.Raycast(ray, out hit) && hit.collider == _colliderToCheck)
+        {
+            // The mouse is over the collider 
+            return true;
+        }
+
+        // Ray did not hit our collider
+        return false;
+    }
+
+
+    /// <summary>
+    /// Returns a string holding interact instructions to the hands of the player <br/>
+    /// Gets the input key for both hands, as well as a text based on the equipment status of the hand itself ("Equip", "Swap") <br/>
+    /// Gets called when new equipment object gets targeted 
+    /// </summary>
+    /// <returns></returns>
+    private string GetInteractionInstructionString()
+    {
+        // Get string based on equip status for both hands
+        string leftHandText = GetEquipOrSwapString(controller.LeftHand); // Uses the references in the EquipmentSystemController to both hands
+        string rightHandText = GetEquipOrSwapString(controller.RightHand); 
+
+        // Make the full string by combining the input keys for the hands, with the equip status, and the type of hand, with a symbol between them for eyecandy 
+        string fullString = $" {controller.LeftHandInputKey} {leftHandText} Left   |   {controller.RightHandInputKey} {rightHandText} Right";
+
+        // Return the full string as Instruction
+        return fullString;
+    }
+
+
+    /// <summary>
+    /// Returns a string containing string to "equip" or "swap", based on equipment status of the param hand <br/>
+    /// Gets called when getting a new Instructions string
+    /// </summary>
+    /// <param name="_hand"> The hand to look at it's CurrentEquipment </param>
+    /// <returns></returns>
+    private string GetEquipOrSwapString(Hand _hand)
+    {
+        // If its null, there is no equipment yet
+        if (_hand.CurrentEquipment == null)
+        {
+            // So return the string holding the word for equip (or similar)
+            return equipActionWord;
+        }
+        else // The hand is holding an equipment
+        {
+            // So return string holding the word for swap (or similar)
+            return swapActionWord;
+        }
     }
 }
