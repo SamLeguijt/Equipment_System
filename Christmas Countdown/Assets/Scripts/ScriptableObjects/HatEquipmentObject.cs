8cf01068eca335464397a05911343c6786144a12
SamLeguijt <sam.leguijt@gmail.com>
2024-01-17 23:36:19 +0100

    Added two new scripts for Hat equipments, will be inherited from Apparel and seperate type of apparel equipments

---
+++ Christmas Countdown/Assets/Scripts/ScriptableObjects/HatEquipmentObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Hat Equipment", menuName = "Equipment/Apparel/Hat")]
+public class HatEquipmentObject : ApparelEquipmentObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
